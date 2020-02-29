5713a53614181178caba4d4f4babcc3d5022d695
jadhlady <56136691+jadhlady@users.noreply.github.com>
2020-02-28 20:56:48 -0800

    changes for fullscreen and cleaning up code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <iostream>
-using namespace std;
 
 int main() {
-
-
-
-    sf::RenderWindow window(sf::VideoMode(640,480,32),"Hello SFML");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 // Display the list of all the video modes available for fullscreen
     std::vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
-    for (std::size_t i = 0; i < modes.size(); ++i)
-    {
+    for (std::size_t i = 0; i < modes.size(); ++i) {
         sf::VideoMode mode = modes[i];
         std::cout << "Mode #" << i << ": "
                   << mode.width << "x" << mode.height << " - "
@@ -19,16 +16,15 @@ int main() {
     }
 // Create a window with the same pixel depth as the desktop
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window",sf::Style::Fullscreen);
+    window.create(sf::VideoMode(1024, 768, desktop.bitsPerPixel), "SFML window", sf::Style::Fullscreen);
 
 
     sf::Font font;
+    if (!font.loadFromFile("../assets/fonts/OpenSans-Bold.ttf")) {
+        return EXIT_FAILURE;
+    }
+    sf::Text text("Hello World", font);
 
-    font.loadFromFile("OpenSans-Bold.ttf");
-
-
-
-    sf::Text text("Hello World",font,11);
 
     text.setCharacterSize(32);
 
@@ -36,36 +32,43 @@ int main() {
 
                      window.getSize().y/2 - text.getGlobalBounds().height/2);
 
-
-
-
+    window.setVerticalSyncEnabled(true); // call it once, after creating the window
 
     while(window.isOpen()){
 
 
-
         sf::Event event;
 
-        while(window.pollEvent(event)) {
+// while there are pending events...
+        while (window.pollEvent(event)) {
+            // check the type of the event...
+            switch (event.type) {
+                // window closed
+                case sf::Event::Closed:
+                    std::cout << "Window has been closed, program ended" << std::endl;
+                    window.close();
+                    break;
 
-            if(event.type == sf::Event::Closed){
+                case sf::Event::KeyPressed:
+                {
 
-                window.close();
 
+
+
+
+
+
+
+                }
             }
-
-
-
-            window.clear(sf::Color::Black);
-
-            window.draw(text);
-
-            window.display();
-
         }
 
-    }
-    cout << "Window has been closed";
-    return 0;
+        window.clear(sf::Color::Black);
 
+        window.draw(text);
+
+        window.display();
+
+        }
+return 0;
 }
